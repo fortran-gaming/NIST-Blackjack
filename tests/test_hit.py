f2b2b38e899257b33e62d1ea653654deda5d8a9e
Michael Hirsch <scivision@users.noreply.github.com>
2020-04-17 14:44:49 -0400

    improve stdin test

--- tests/test_hit.py
+++ tests/test_hit.py
@@ -2,18 +2,22 @@
 import subprocess
 from pathlib import Path
 import pytest
+import shutil
 
 R = Path(__file__).resolve().parents[1] / 'build'
 
 
 def test_c():
 
-    proc = subprocess.Popen('./c_blackjack', cwd=R, text=True,
-                            stdin=subprocess.PIPE)
+    exe = shutil.which('c_blackjack', path=str(R))
+    if exe is None:
+        raise FileNotFoundError("C blackjack not found")
+
+    proc = subprocess.Popen(exe, cwd=R, text=True, stdin=subprocess.PIPE)
 
     ret = None
     while ret is None:
-        proc.stdin.write('y\n')
+        proc.communicate(input='y\n')
         ret = proc.poll()
 
     assert ret == 0
@@ -21,16 +25,19 @@ def test_c():
 
 def test_fortran():
 
-    proc = subprocess.Popen('./f_blackjack', cwd=R, text=True,
-                            stdin=subprocess.PIPE)
+    exe = shutil.which('f_blackjack', path=str(R))
+    if exe is None:
+        raise FileNotFoundError("Fortran blackjack not found")
+
+    proc = subprocess.Popen(exe, text=True, stdin=subprocess.PIPE)
 
     ret = None
     while ret is None:
-        proc.stdin.write('y\n')
+        proc.communicate(input='y\n')
         ret = proc.poll()
 
     assert ret == 0
 
 
 if __name__ == '__main__':
-    pytest.main(['-v', __file__])
+    pytest.main([__file__])
