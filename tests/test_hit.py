3e999518a2ea0dfd8765aa938c23043d8e0995d4
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-26 01:23:46 -0400

    simplify test

--- tests/test_hit.py
+++ tests/test_hit.py
@@ -1,43 +1,18 @@
 #!/usr/bin/env python
 import subprocess
 from pathlib import Path
-import pytest
-import shutil
-
-R = Path(__file__).resolve().parents[1] / 'build'
+import sys
 
 
-def test_c():
+exe = sys.argv[1]
+if not Path(exe).is_file():
+    raise FileNotFoundError(f"{exe} not found")
 
-    exe = shutil.which('c_blackjack', path=str(R))
-    if exe is None:
-        raise FileNotFoundError("C blackjack not found")
+proc = subprocess.Popen(exe, text=True, stdin=subprocess.PIPE)
 
-    proc = subprocess.Popen(exe, cwd=R, text=True, stdin=subprocess.PIPE)
+ret = None
+while ret is None:
+    proc.communicate(input='y\n')
+    ret = proc.poll()
 
-    ret = None
-    while ret is None:
-        proc.communicate(input='y\n')
-        ret = proc.poll()
-
-    assert ret == 0
-
-
-def test_fortran():
-
-    exe = shutil.which('f_blackjack', path=str(R))
-    if exe is None:
-        raise FileNotFoundError("Fortran blackjack not found")
-
-    proc = subprocess.Popen(exe, text=True, stdin=subprocess.PIPE)
-
-    ret = None
-    while ret is None:
-        proc.communicate(input='y\n')
-        ret = proc.poll()
-
-    assert ret == 0
-
-
-if __name__ == '__main__':
-    pytest.main([__file__])
+assert ret == 0
